5fd1061966ca722270f4ec4429c412e2eebbfc40
Gus-Eisen <guseisenberch@proton.me>
2026-05-09 12:39:20 -0500

    Path reolution fix. Switched to include_bytes macro.

--- src/objects/windows_obj.rs
+++ src/objects/windows_obj.rs
@@ -1,13 +1,8 @@
-use quartz::{load_image_sized, tint_overlay, Arc, Font, Text, Span, Align, Color, Image};
 use crate::preferences::*;
 use crate::rampstack::windows::*;
 use flowmango::GameObject;
 use quartz::Canvas;
-
-fn read_image_sized(path: &str, w: f32, h: f32) -> Image {
-    let bytes = std::fs::read(path).expect(&format!("missing image: {}", path));
-    load_image_sized(&bytes, w, h)
-}
+use quartz::{load_image_sized, tint_overlay, Align, Arc, Color, Font, Image, Span, Text};
 
 pub fn ph_text(glyph: &str, size: f32, color: Color, font: Arc<Font>) -> Text {
     Text::new(
@@ -26,54 +21,111 @@ pub fn ph_text(glyph: &str, size: f32, color: Color, font: Arc<Font>) -> Text {
 }
 
 pub fn setup(
-    cv:       &mut Canvas,
-    init_cw:  f32,
-    init_ch:  f32,
-    ratio_a:  f32,
-    ratio_b:  f32,
+    cv: &mut Canvas,
+    init_cw: f32,
+    init_ch: f32,
+    ratio_a: f32,
+    ratio_b: f32,
     fa_solid: Arc<Font>,
     ph_light: Arc<Font>,
 ) {
-    let split_a   = (init_cw * ratio_a).round();
-    let split_b   = (init_cw * ratio_b).round();
+    let split_a = (init_cw * ratio_a).round();
+    let split_b = (init_cw * ratio_b).round();
     let panel_top = TOPBAR_H + 1.0;
-    let panel_h   = init_ch - panel_top;
+    let panel_h = init_ch - panel_top;
 
-    cv.add_game_object("app_bg".into(), GameObject::build("app_bg")
-        .position(0.0, 0.0).size(4000.0, 4000.0).layer(-1)
-        .image(tint_overlay(4000.0, 4000.0, COL_APP_BG)).finish());
+    cv.add_game_object(
+        "app_bg".into(),
+        GameObject::build("app_bg")
+            .position(0.0, 0.0)
+            .size(4000.0, 4000.0)
+            .layer(-1)
+            .image(tint_overlay(4000.0, 4000.0, COL_APP_BG))
+            .finish(),
+    );
 
-    cv.add_game_object("topbar_bg".into(), GameObject::build("topbar_bg")
-        .position(0.0, 0.0).size(init_cw, TOPBAR_H).layer(5)
-        .image(tint_overlay(init_cw, TOPBAR_H, COL_TOPBAR_BG)).finish());
+    cv.add_game_object(
+        "topbar_bg".into(),
+        GameObject::build("topbar_bg")
+            .position(0.0, 0.0)
+            .size(init_cw, TOPBAR_H)
+            .layer(5)
+            .image(tint_overlay(init_cw, TOPBAR_H, COL_TOPBAR_BG))
+            .finish(),
+    );
 
-    cv.add_game_object("topbar_sep".into(), GameObject::build("topbar_sep")
-        .position(0.0, TOPBAR_H).size(init_cw, 1.0).layer(6)
-        .image(tint_overlay(init_cw, 1.0, COL_BORDER)).finish());
+    cv.add_game_object(
+        "topbar_sep".into(),
+        GameObject::build("topbar_sep")
+            .position(0.0, TOPBAR_H)
+            .size(init_cw, 1.0)
+            .layer(6)
+            .image(tint_overlay(init_cw, 1.0, COL_BORDER))
+            .finish(),
+    );
 
     let logo_y = (TOPBAR_H - LOGO_H) * 0.5;
-    cv.add_game_object("topbar_logo".into(), GameObject::build("topbar_logo")
-        .position(LOGO_PAD, logo_y).size(LOGO_W, LOGO_H).layer(7)
-        .image(read_image_sized("resources/rampstacklogo.png", LOGO_W, LOGO_H))
-        .finish());
+    cv.add_game_object(
+        "topbar_logo".into(),
+        GameObject::build("topbar_logo")
+            .position(LOGO_PAD, logo_y)
+            .size(LOGO_W, LOGO_H)
+            .layer(7)
+            .image(load_image_sized(
+                include_bytes!("../../resources/rampstacklogo.png"),
+                LOGO_W,
+                LOGO_H,
+            ))
+            .finish(),
+    );
 
     let rects = icon_rects(init_cw);
-    cv.add_game_object("icon_stacked".into(), GameObject::build("icon_stacked")
-        .position(rects[0].0, rects[0].1).size(ICON_SIZE, ICON_SIZE).layer(7)
-        .image(read_image_sized("resources/unselected_stacked.png", ICON_SIZE, ICON_SIZE))
-        .finish());
-    cv.add_game_object("icon_sidebyside".into(), GameObject::build("icon_sidebyside")
-        .position(rects[1].0, rects[1].1).size(ICON_SIZE, ICON_SIZE).layer(7)
-        .image(read_image_sized("resources/selected_sidebyside.png", ICON_SIZE, ICON_SIZE))
-        .finish());
+    cv.add_game_object(
+        "icon_stacked".into(),
+        GameObject::build("icon_stacked")
+            .position(rects[0].0, rects[0].1)
+            .size(ICON_SIZE, ICON_SIZE)
+            .layer(7)
+            .image(load_image_sized(
+                include_bytes!("../../resources/unselected_stacked.png"),
+                ICON_SIZE,
+                ICON_SIZE,
+            ))
+            .finish(),
+    );
+    cv.add_game_object(
+        "icon_sidebyside".into(),
+        GameObject::build("icon_sidebyside")
+            .position(rects[1].0, rects[1].1)
+            .size(ICON_SIZE, ICON_SIZE)
+            .layer(7)
+            .image(load_image_sized(
+                include_bytes!("../../resources/selected_sidebyside.png"),
+                ICON_SIZE,
+                ICON_SIZE,
+            ))
+            .finish(),
+    );
 
-    cv.add_game_object("sidebar_bg".into(), GameObject::build("sidebar_bg")
-        .position(0.0, panel_top).size(SIDEBAR_W, panel_h).layer(4)
-        .image(tint_overlay(SIDEBAR_W, panel_h, COL_SIDEBAR_BG)).finish());
+    cv.add_game_object(
+        "sidebar_bg".into(),
+        GameObject::build("sidebar_bg")
+            .position(0.0, panel_top)
+            .size(SIDEBAR_W, panel_h)
+            .layer(4)
+            .image(tint_overlay(SIDEBAR_W, panel_h, COL_SIDEBAR_BG))
+            .finish(),
+    );
 
-    cv.add_game_object("sidebar_sep".into(), GameObject::build("sidebar_sep")
-        .position(SIDEBAR_W, panel_top).size(1.0, panel_h).layer(4)
-        .image(tint_overlay(1.0, panel_h, COL_BORDER)).finish());
+    cv.add_game_object(
+        "sidebar_sep".into(),
+        GameObject::build("sidebar_sep")
+            .position(SIDEBAR_W, panel_top)
+            .size(1.0, panel_h)
+            .layer(4)
+            .image(tint_overlay(1.0, panel_h, COL_BORDER))
+            .finish(),
+    );
 
     let icon_x = (SIDEBAR_W - SIDEBAR_ICON_SIZE) * 0.5;
 
@@ -91,7 +143,10 @@ pub fn setup(
     cv.add_game_object("sidebar_icon_files".into(), file_obj);
 
     let mut search_obj = GameObject::build("sidebar_icon_search")
-        .position(icon_x, panel_top + SIDEBAR_ICON_TOP + SIDEBAR_ICON_SIZE + SIDEBAR_ICON_GAP)
+        .position(
+            icon_x,
+            panel_top + SIDEBAR_ICON_TOP + SIDEBAR_ICON_SIZE + SIDEBAR_ICON_GAP,
+        )
         .size(SIDEBAR_ICON_SIZE, SIDEBAR_ICON_SIZE)
         .layer(8)
         .finish();
@@ -104,29 +159,39 @@ pub fn setup(
     cv.add_game_object("sidebar_icon_search".into(), search_obj);
 
     let mut div_a = GameObject::build("divider_a")
-        .position(split_a, panel_top).size(DIV_W, panel_h).layer(10).finish();
+        .position(split_a, panel_top)
+        .size(DIV_W, panel_h)
+        .layer(10)
+        .finish();
     div_a.set_image(divider_image_v(panel_h));
     cv.add_game_object("divider_a".into(), div_a);
 
     let mut div_b = GameObject::build("divider_b")
-        .position(split_b, panel_top).size(DIV_W, panel_h).layer(10).finish();
+        .position(split_b, panel_top)
+        .size(DIV_W, panel_h)
+        .layer(10)
+        .finish();
     div_b.set_image(divider_image_v(panel_h));
     cv.add_game_object("divider_b".into(), div_b);
 
-    let right_w      = init_cw - split_a - DIV_W;
+    let right_w = init_cw - split_a - DIV_W;
     let init_ratio_c = 0.6f32;
-    let split_c_y    = panel_top + (panel_h * init_ratio_c).round();
+    let split_c_y = panel_top + (panel_h * init_ratio_c).round();
     let mut div_c = GameObject::build("divider_c")
-        .position(split_a + DIV_W, split_c_y).size(right_w, DIV_W).layer(10).finish();
+        .position(split_a + DIV_W, split_c_y)
+        .size(right_w, DIV_W)
+        .layer(10)
+        .finish();
     div_c.set_image(divider_image_h(right_w));
     div_c.visible = false;
     cv.add_game_object("divider_c".into(), div_c);
 
-    cv.set_var("ratio_a",        quartz::Value::from(ratio_a));
-    cv.set_var("ratio_b",        quartz::Value::from(ratio_b));
-    cv.set_var("ratio_c",        quartz::Value::from(init_ratio_c));
-    cv.set_var("drag_which",     quartz::Value::from(0u8));
-    cv.set_var("layout_mode",    quartz::Value::from(0u8));
-    cv.set_var("min_explorer",   quartz::Value::from(MIN_EXPLORER));
-    cv.set_var("sidebar_active", quartz::Value::from(SIDEBAR_FILES)); 
+    cv.set_var("ratio_a", quartz::Value::from(ratio_a));
+    cv.set_var("ratio_b", quartz::Value::from(ratio_b));
+    cv.set_var("ratio_c", quartz::Value::from(init_ratio_c));
+    cv.set_var("drag_which", quartz::Value::from(0u8));
+    cv.set_var("layout_mode", quartz::Value::from(0u8));
+    cv.set_var("min_explorer", quartz::Value::from(MIN_EXPLORER));
+    cv.set_var("sidebar_active", quartz::Value::from(SIDEBAR_FILES));
 }
+
