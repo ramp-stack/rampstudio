f4e47a87af2bed2d9cef65c24f504947b715284a
Gus-Eisen <guseisenberch@proton.me>
2026-05-19 16:58:15 -0500

    Fix: lock mutex before calling methods on editor field .

--- src/components/editor.rs
+++ src/components/editor.rs
@@ -1,48 +1,57 @@
-use editor::Editor;
 use editor::prelude::Settings;
+use editor::Editor;
 use flowmango::Canvas;
 use quartz::{Arc, Font};
+use std::sync::Mutex;
 
 #[derive(Clone)]
 pub struct EditorComponent {
-    pub inner: Arc<Editor>,
+    pub inner: Arc<Mutex<Editor>>,
 }
 
 impl EditorComponent {
     pub fn new(
-        x: f32, y: f32, w: f32, h: f32,
-        code_font:    Arc<Font>,
-        gutter_font:  Arc<Font>,
+        x: f32,
+        y: f32,
+        w: f32,
+        h: f32,
+        code_font: Arc<Font>,
+        gutter_font: Arc<Font>,
         initial_file: &str,
-        theme_bytes:  &[u8],
-        settings:     Settings,
+        theme_bytes: &[u8],
+        settings: Settings,
     ) -> Self {
         Self {
-            inner: Arc::new(Editor::new(
-                x, y, w, h,
+            inner: Arc::new(Mutex::new(Editor::new(
+                x,
+                y,
+                w,
+                h,
                 code_font,
                 gutter_font,
                 initial_file,
                 theme_bytes,
                 settings,
-            )),
+            ))),
         }
     }
 
     pub fn mount(&self, cv: &mut Canvas) {
-        self.inner.mount(cv);
-        self.inner.register_callbacks(cv);
+        self.inner.lock().unwrap().mount(cv);
+        self.inner.lock().unwrap().register_callbacks(cv);
     }
 
     pub fn set_bounds(&self, x: f32, y: f32, w: f32, h: f32) {
-        self.inner.set_bounds(x, y, w, h);
+        self.inner.lock().unwrap().set_bounds(x, y, w, h);
     }
 
     pub fn bounds(&self) -> (f32, f32, f32, f32) {
-        self.inner.get_bounds()
+        self.inner.lock().unwrap().get_bounds()
     }
 
     pub fn set_focus(&self, focused: bool) {
-        self.inner.set_focus(focused);
+        let mut editor = self.inner.lock().unwrap();
+        editor.set_focus(focused);
     }
 }
+
